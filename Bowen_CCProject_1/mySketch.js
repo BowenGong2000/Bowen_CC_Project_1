62c2943cebb9ba4ee3aa3fecde306bbc3cc7b0c7
Bowen Gong <67953712+BowenGong2000@users.noreply.github.com>
2021-03-20 01:59:47 +0800

    Update mySketch.js

--- Bowen_CCProject_1/mySketch.js
+++ Bowen_CCProject_1/mySketch.js
@@ -13,7 +13,12 @@ function setup() {
 
 function draw() {
 	fill('#191970');
+	rectMode(CENTER);
 	rect(windowWidth/2, windowHeight/2,650,650)
 	fill('#000000')
+	quad(443,50,463,80,463,630,443,660);
+	rectMode(CORNER);
+	rect(443,50,20,30);
+	quad(463,50,535,90,515,100,463,80); 
+	quad(535,90,535,600,515,570,515,100); 
 }
-
