7b2d65942d289e3c4f44e8390acde534992d01f0
Bowen Gong <67953712+BowenGong2000@users.noreply.github.com>
2021-03-19 21:05:00 +0800

    Update mySketch.js
    set background

--- Bowen_CCProject_1/mySketch.js
+++ Bowen_CCProject_1/mySketch.js
@@ -1,9 +1,19 @@
+// CC Project 1
+// Bowen Gong bg1941
+
+//Global variables
+var length = 10;
+
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	background(100);
+	background('#FFFFFF');  //black #000000
+	rectMode(CENTER);
+
 }
 
 function draw() {
-	ellipse(mouseX, mouseY, 20, 20);
-	rect(100,100,50,50);
+	fill('#191970');
+	rect(windowWidth/2, windowHeight/2,650,650)
+	fill('#000000')
 }
+
