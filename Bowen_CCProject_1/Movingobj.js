bc6035186dcf1aa60cb098e1ccd301035c600733
Bowen Gong <67953712+BowenGong2000@users.noreply.github.com>
2021-04-06 19:27:26 +0800

    add "ghost" shadow
    change circle shadow to a ghost-like shadow, allow its size to increase as the shadow get closer and closer to the window

--- Bowen_CCProject_1/Movingobj.js
+++ Bowen_CCProject_1/Movingobj.js
@@ -2,36 +2,54 @@ class Movingobj{
 	constructor(){
 		this.x = windowwidth/2;
 		this.y = 400;
-		this.yspeed = 0.1
+		this.yspeed = 0.1;
 		this.sizeX = 10;
-		this.sizeY = 10;
-		this.addsizeX = 0.08;
-		this.addsizeY = 0.08;
+		this.sizeY = 20;
+		this.addsizeX = 0.2;
+		this.addsizeY = 0.2;
+		this.accleration = 0.001;
+		this.eyesize = 10;
+		this.addeyesize = 0.01;
+		this.eye_dist = 5;
+		this.add_dist = 0.01;
 	}
 	move(){
 		this.y = this.y + this.yspeed;
+		this.yspeed = this.yspeed + this.accleration;
 		this.sizeX = this.sizeX + this.addsizeX;
 		this.sizeY = this.sizeY + this.addsizeY;
+		this.eyesize = this.eyesize + this.addeyesize;
+		this.eye_dist = this.eye_dist + this.add_dist;
 	}
 	display(){
 		noStroke();
 		fill(0,0,0,200);
-		ellipse(this.x, this.y, this.sizeX,this.sizeY);
+		ellipse(this.x, this.y, this.sizeX,this.sizeY);	
+		//circle(this.x-this.eye_dist*2,this.y,20);
+		//circle(this.x+this.eye_dist*2,this.y,20)
+		
+		fill(139,0,0);
+		circle(this.x-this.eye_dist,this.y-13,this.eyesize);
+		circle(this.x+this.eye_dist,this.y-13,this.eyesize);
 	}
 	stop(){
 		if(this.y>610){
 			this.yspeed = 0;
 			this.addsizeX = 0;
 			this.addsizeY = 0;
+			
 		}
 	}
 	reset(){
-		this.sizeX = 10;
-		this.sizeY = 10;
+		this.sizeX = 30;
+		this.sizeY = 40;
 		this.y = 400;
 		this.yspeed = 0.1;
-		this.addsizeX = 0.06;
-		this.addsizeY = 0.06;
+		this.addsizeX = 0.2;
+		this.addsizeY = 0.23;
+		this.eyesize = 10;
+		this.eye_dist = 5;
+		this.add_dist = 0.01;
 	}
 	draw_face(){
 		if(this.y>610){
